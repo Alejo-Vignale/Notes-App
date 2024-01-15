f38079f6b42d368fe5991b00a01a1cdfa7afc326
Alejo-Vignale <138889322+Alejo-Vignale@users.noreply.github.com>
2024-01-15 17:56:58 -0300

    app notes finished

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const notesContainer= document.querySelector(".notes-container");
-const createBtn= document.querySelector(".btn");
-
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-createBtn.addEventListener("click",()=>{
-    let inputBox = document.createElement("p");
-    let img= document.createElement("img");
-    inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable", "true");
-    img.src= "images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
-})
+//if anything is displayed on the browser it will show the notes
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
 
-notesContainer.addEventListener("click",function(e){
-    if(e.target.tagName ==="IMG"){
-        e.target.parentElement.remove();
-    }
-})
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } //so that it updates evrything on the p tag
+  else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
